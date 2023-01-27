4c620190962fd111252469fa1b50a48e059175fe
cwilmot <chazwilmot@gmail.com>
2023-01-27 12:59:17 -0600

    Numpy Operations Notes

---
+++ Notes/Numpy Operations.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+arr = np.arange(0,11)
+
+
+# In[3]:
+
+
+arr
+
+
+# In[4]:
+
+
+arr + arr
+
+
+# In[5]:
+
+
+arr - arr
+
+
+# In[6]:
+
+
+arr * arr
+
+
+# In[8]:
+
+
+arr + 100
+
+
+# In[9]:
+
+
+arr * 100
+
+
+# In[10]:
+
+
+arr / 100
+
+
+# In[11]:
+
+
+arr - 100
+
+
+# In[13]:
+
+
+arr / ar
+
+
+# In[14]:
+
+
+1 / arr
+
+
+# In[15]:
+
+
+arr ** 2
+
+
+# In[16]:
+
+
+# Universal Array Functions
+
+
+# In[17]:
+
+
+np.sqrt(arr)
+
+
+# In[18]:
+
+
+np.exp(arr)
+
+
+# In[19]:
+
+
+np.max(arr)
+
+
+# In[20]:
+
+
+arr.max()
+
+
+# In[21]:
+
+
+np.sin(arr)
+
+
+# In[22]:
+
+
+np.log(arr)
+
+
+# In[23]:
+
+
+# ufunc
+
